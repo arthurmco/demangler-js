7a0aa07a3b4bca3a90cd55be0cf6737f445c8e47
Arthur Mendes <arthurmco@gmail.com>
2018-06-12 00:06:19 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/**
+
+   Demangler for C++ function names, written in Javascript
+   Copyright (C) Arthur Mendes, licensed under the MIT license
+
+**/
+
+module.exports = {
+
+    demangle: function(fname) {
+	
+    }
+    
+};
---
+++ src/itanium-abi.js
@@ -0,0 +1,19 @@
+/**
+   Demangles C++ function names mangled according to the IA64 C++ ABI
+
+   This is a pretensious and cocky way to say that this file demangles function names
+   mangled by GCC and Clang.
+**/
+
+module.exports = {
+
+    /* Check if the name passed is a IA64 ABI mangled name */
+    isMangled: function(name) {
+	
+    },
+
+    demangle: function(name) {
+	
+    }
+    
+};
