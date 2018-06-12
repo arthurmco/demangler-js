93ba6d3c795c795a20cbd665fafb784d18135010
Arthur Mendes <arthurmco@gmail.com>
2018-06-12 14:14:59 -0300

    Fix bug where the package did not work

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const itanium_abi = require('./itanium-abi.js');
 module.exports = {
 
     demangle: function(fname) {
-	if (!itanium_abi.isMangled(fname))
+	if (itanium_abi.isMangled(fname))
 	    return itanium_abi.demangle(fname);
 
 	return fname;
