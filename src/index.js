1fe69b8ac862d7cdc8d22db089f90b701297bf41
Arthur Mendes <arthurmco@gmail.com>
2018-06-12 04:22:25 -0300

    Add the demangler into the entry point
    Now you can use it!

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@
 
 **/
 
+const itanium_abi = require('./itanium-abi.js');
+
 module.exports = {
 
     demangle: function(fname) {
-	
+	if (!itanium_abi.isMangled(fname))
+	    return itanium_abi.demangle(fname);
+
+	return fname;
     }
     
 };
